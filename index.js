c9e0cf26eb28a804978cd7d7ae43abd6cf90e248
Tanvir <zishantanvir05@gmail.com>
2025-02-16 20:19:38 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req,res) => {
+    res.send("Job is falling from the sky")
+})
+app.listen(port, ()=> {
+    console.log("Server is running on port: ", port)
+})
