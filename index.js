e4f448477587808c413fa3b0eb86f1304b535b3d
Tanvir <zishantanvir05@gmail.com>
2025-02-17 22:09:30 +0600

    add jobs application apis

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 app.use(cors());
 app.use(express.json());
@@ -29,12 +29,26 @@ async function run() {
             "Pinged your deployment. You successfully connected to MongoDB!"
         );
 
-        // Jobs Related Apis 
+        // Jobs Related Apis
         const JobsCollection = client.db("jobPortalA").collection("Jobs");
-        
-        app.get('/jobs',async (req,res) => {
+        const jobApplicationCollection = client.db("jobPortalA").collection("job_application")
+
+        app.get("/jobs", async (req, res) => {
             const cursor = JobsCollection.find();
             const result = await cursor.toArray();
+            res.send(result);
+        });
+        app.get("/jobs/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await JobsCollection.findOne(query);
+            res.send(result);
+        });
+
+        // job application apis 
+        app.post('/job-application', async(req,res)=> {
+            const application = req.body;
+            const result = await jobApplicationCollection.insertOne(application)
             res.send(result)
         })
 
