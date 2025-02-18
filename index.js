d4fc35556e5e61594af089d7077dd2e5e80842c4
Tanvir <zishantanvir05@gmail.com>
2025-02-18 10:23:42 +0600

    module 59 finally complete

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ async function run() {
                     application.title = job.title;
                     application.company = job.company;
                     application.company_logo = job.company_logo;
+                    application.location = job.location;
                 }
             }
 
